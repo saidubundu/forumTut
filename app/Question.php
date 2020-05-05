94f7b1d1a05df410334769c443edf29eca403ced
saidubundu <34603026+saidubundu@users.noreply.github.com>
2020-05-05 16:36:29 +0000

    First phase of Question Schema Commpleted

---
+++ app/Question.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Parsedown;
+
+class Question extends Model
+{
+    //
+    protected $fillable=['title','body'];
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function setTitleAttribute($value)
+    {
+        $this->attributes['title'] = $value;
+        $this->attributes['slug'] = str_slug($value);
+    }
+
+    public function getUrlAttribute()
+    {
+        return route("questions.show", $this->slug);
+    }
+
+    public function getCreatedDateAttribute()
+    {
+        return $this->created_at->diffForHumans();
+    }
+
+    public function getStatusAttribute()
+    {
+        if ($this->answers > 0){
+            if ($this->best_answered_id){
+                return "answered-accepted";
+            }
+
+            return "answered";
+        }
+        return "unanswered";
+    }
+
+    public function getBodyHtmlAttribute()
+    {
+        return Parsedown::instance()->text($this->body);
+    }
+}
